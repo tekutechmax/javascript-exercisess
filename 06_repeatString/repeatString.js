cc8b2c0c35e583a31d1b6e24525011f23434dad2
tekutechmax <birhanuteku390@gmail.com>
2025-07-13 12:16:50 +0300

    Update repeatString.js

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
+  string = "";
+  
+  for (let i =0;i<num;i++){
+    string +=str
 
-};
+}
+  return string ;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
